45ecb7c704c769d567ee1700dc67027e7a69be67
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-06 21:48:47 -0400

    formatting

--- bdd/bdd.sql
+++ bdd/bdd.sql
@@ -99,13 +99,6 @@ VALUES (1, 'giraffe.png', 'Un dessin d''une girafe', 1),
        (8, 'troisAmis.jpg', 'Une photo vieille des trois amis', 4);
 
 -- --------------------------------------------------------
---
--- Exporter les tables
---
-
-
-
--- --------------------------------------------------------
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
