2abfe8209b758ff4fb3907d8037a5be637d821d2
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-08 19:46:40 -0400

    fixer sql variable probleme essaie3

--- index.php
+++ index.php
@@ -85,13 +85,14 @@ if (isset($_POST['connexion_util'])) {
 function category(string $cat, $link) {
     $catId = executeQuery($link,"SELECT catId FROM Categorie WHERE nomCat = '$cat'");
     $result = executeQuery($link, "SELECT nomFich FROM Photo WHERE catId = '$catId'");
-    $nomImages = mysqli_fetch_array($result); //row
-    
-    foreach($nomImages as $image):
-        $im = glob($GLOBALS['dir'] . $image, GLOB_BRACE);
-        echo "<img src='" . $im . "' />";
-    endforeach;
 
+    echo "split up now";
+
+    while($row = mysqli_fetch_array($result))
+        echo "!";
+        $im = glob($GLOBALS['dir'] . $row['nomFich'], GLOB_BRACE);
+        echo "<img src='" . $im . "' />";
+    echo "fini?";
 }
 if ($catId = 5) {
     $images = glob($dir. '*.{png,jpg,gif}', GLOB_BRACE);
