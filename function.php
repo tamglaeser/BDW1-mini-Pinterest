613fd7dc890f1e1f4f572493575145952aa8ec5f
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-09 11:32:06 -0400

    fix function

--- function.php
+++ function.php
@@ -15,14 +15,13 @@ $dir = "assets/images/";
 function category(int $cat, $link) {
     echo "la category: ".$cat;
     $resultat = executeQuery($link, "SELECT nomFich FROM Photo WHERE catId = $cat");
-    echo $resultat;
     //$sql = "SELECT nomFich FROM Photo WHERE catId = $cat";
     //$resultat = $link->query($sql);
-    /*while($row = $resultat->fetch_assoc()) {
+    while($row = $resultat->fetch_assoc()) {
         echo "nomFich: " . $row["nomFich"] . " ";
         //$im = glob($GLOBALS['dir'] . $nomFich, GLOB_BRACE);
         //echo "<img src='" . $im . "' />";
-    }*/
+    }
 }
 
 ?>
