39dee372cf9a906092fe79dca514e2e709477d5c
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-09 12:06:58 -0400

    small error

--- function.php
+++ function.php
@@ -3,6 +3,7 @@
 if (isset($_POST['show_dowpdown_value'])) {
 
     $catId = $_POST['dowpdown']; // this will print the value if downbox out
+    $dir = "assets/images/";
     if ($catId != 0) {
         category($catId, $conn);
     }
@@ -20,7 +21,7 @@ if (isset($_POST['show_dowpdown_value'])) {
 require_once('bd.php');
 $conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
 
-$dir = "assets/images/";
+
 
 function category(int $cat, $link) {
     $resultat = executeQuery($link, "SELECT nomFich FROM Photo WHERE catId = $cat");
