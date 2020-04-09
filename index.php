6b615a90aa821aaa6b9e4d2e58f3d43f445ec934
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-08 20:21:03 -0400

    debugging

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ if (isset($_POST['connexion_util'])) {
 
 function category(string $cat, $link) {
     $catId = executeQuery($link,"SELECT catId FROM Categorie WHERE nomCat = '$cat'");
-    echo $catId;
+    echo (string)$catId;
     /*
     $sql = "SELECT nomFich FROM Photo WHERE catId = $catId";
     $resultat = $link->query($sql);
