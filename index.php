bf5322520c61eacbdccb410532ad60aec3b645af
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-09 11:52:19 -0400

    move headers to top again

--- index.php
+++ index.php
@@ -14,29 +14,6 @@ $conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
 $dir = "assets/images/";
 $catId=5;
 
-
-session_start();
-
-if (isset($_POST['accueil'])) {
-    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/index.php');
-    exit();
-}
-if (isset($_POST['connexion_ad'])) {
-    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/connexion_admin.php');
-    exit();
-}
-if (isset($_POST['connexion_util'])) {
-    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/connexion_utilisateur.php');
-    exit();
-}
-
-$images = glob($dir. '*.{png,jpg,gif}', GLOB_BRACE);
-foreach ($images as $image):
-    echo "<img src='" . $image . "' />";
-
-endforeach;
-
-
 ?>
 <div style="background-image:url(img/accueil_bis.jpg);" ><B><h1>PhotoCat</h1></B><br> </div>
 	<nav class="crumbs">
@@ -54,7 +31,6 @@ endforeach;
 		</div>
 	</nav>
     <h1>Toutes les photos</h1>
-
 <form action="function.php" method="post">
     <!-- here start the dropdown list -->
     <select name="dowpdown">
@@ -66,8 +42,32 @@ endforeach;
     <input type="submit" name="show_dowpdown_value" value="Valider"/>
 </form>
 <?php include("function.php"); ?>
+
 </body>
 </html>
+
 <?php
+
+session_start();
+
+if (isset($_POST['accueil'])) {
+    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/index.php');
+    exit();
+}
+if (isset($_POST['connexion_ad'])) {
+    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/connexion_admin.php');
+    exit();
+}
+if (isset($_POST['connexion_util'])) {
+    header('Location: https://bdw1.univ-lyon1.fr/p1501149/Projet/src/connexion_utilisateur.php');
+    exit();
+}
+if ($catId = 5) {
+    $images = glob($dir. '*.{png,jpg,gif}', GLOB_BRACE);
+    foreach ($images as $image):
+        echo "<img src='" . $image . "' />";
+
+    endforeach;
+}
 closeConnexion($conn);
 ?>
