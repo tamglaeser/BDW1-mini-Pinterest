c1259ab935efbf8fc19082ff9b7abe1f02c208b5
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-14 15:18:50 -0400

    testing

--- details.php
+++ details.php
@@ -1,4 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PhotouCat</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
 <?php
+echo "ciao";
 function details($nomIm, $link) {
     $resultat = executeQuery($link, "SELECT description, catId FROM Photo WHERE nomFich=$nomIm");
     $src = glob($GLOBALS['dir'] .$nomIm, GLOB_BRACE);
@@ -6,3 +17,6 @@ function details($nomIm, $link) {
 
 }
 ?>
+
+</body>
+</html>
