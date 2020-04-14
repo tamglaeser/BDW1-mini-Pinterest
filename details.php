7f16cdf142b49d0014d46d5c69fd200639f27513
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-14 17:58:03 -0400

    test without forloop

--- details.php
+++ details.php
@@ -12,8 +12,11 @@
 <?php
 echo $_GET['photoId'];
 $photoId = $_GET['photoId'];
-function details($nomIm, $link) {
-    $photoId = $_GET['photoId'];
+$conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
+
+details($photoId, $conn);
+
+function details($ImageId, $link) {
     $resultat = executeQuery($link, "SELECT description, catId FROM Photo WHERE nomFich=$nomIm");
     $src = glob($GLOBALS['dir'] .$nomIm, GLOB_BRACE);
     echo "<img src='" . $src . "' hspace = '10' border = '5'/>";
