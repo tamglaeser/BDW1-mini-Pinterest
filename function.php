f7d6518dccad58fe5aed321ce5a5dd79539cdbfd
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-16 14:11:09 -0400

    clean up code

--- function.php
+++ function.php
@@ -17,24 +17,7 @@ $dir = "assets/images/";
 if (isset($_POST['show_dowpdown_value']) and $_POST['dowpdown'] !=0) {
 
     $catId = $_POST['dowpdown']; // this will print the value if downbox out
-    //$dir = "assets/images/";
-    /*if ($catId != 0) {*/
-        category($catId, $GLOBALS['conn']);
-    /*}
-    else {
-
-        ?>
-        <h1>Toutes les photos</h1><?php
-        $resultat_photoId = executeQuery($GLOBALS['conn'], "SELECT photoId FROM Photo");
-        while ($row_photoId = $resultat_photoId->fetch_assoc()) {
-            $resultat_imNom = executeQuery($GLOBALS['conn'], "SELECT nomFich FROM Photo WHERE photoId = " . $row_photoId["photoId"] );
-            $row_imNom = $resultat_imNom->fetch_assoc();
-            $images = glob($GLOBALS['dir'] . $row_imNom["nomFich"], GLOB_BRACE);
-            foreach ($images as $image):
-                echo "<a href='details.php?photoId=" . $row_photoId["photoId"] . "'><img src='" . $image . "' hspace = '10' border = '5'/></a>";
-            endforeach;
-        }
-    }*/
+    category($catId, $GLOBALS['conn']);
 }
 
 if (isset($_GET['catId'])) {
