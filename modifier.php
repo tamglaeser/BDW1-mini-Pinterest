3f1a9a90b6dfaa68b7436fd352539024aa5e2014
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-18 13:02:37 -0400

    debug

--- modifier.php
+++ modifier.php
@@ -99,6 +99,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
+echo "BEFORE $-POST SUBMIT??";
 if(isset($_POST["submit"])) {
     echo "SUBMITTED";
 
