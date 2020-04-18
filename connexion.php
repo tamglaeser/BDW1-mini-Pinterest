9dcf381344e791cc677b1071fbaaf50a56574f0a
MASINGARBE MARINE p1501149 <marine.masingarbe@etu.univ-lyon1.fr>
2020-04-18 19:30:50 +0200

    Upload New File

---
+++ connexion.php
@@ -0,0 +1,170 @@
+<?php
+	require_once('bd.php');
+	require_once('utilisateur.php');
+	require_once('administrateur.php');
+/*bdw1.univ-lyon1.fr/p1501149/tp4*/
+
+?>
+
+<?php
+session_start();
+ if (isset($_POST['accueil'])) {
+	header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/accueil.php');
+	exit();
+}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Bienvenue sur PhotouCat</title>
+  <link rel="stylesheet" href="bootstrap.css">
+  <link rel="stylesheet" href="accueil.css">
+</head>
+<body>
+<div style="background-image:url(img/accueil_bis.jpg);" ><B><h1>PhotouCat</h1></B><br> </div>
+<nav class="crumbs">
+	<form name="accueil" action="accueil.php" method="POST">
+	   <button style="float: left;" type="submit" name="accueil" class="btn btn-success">
+		Accueil
+		</button>
+	</nav>
+	<?php
+
+/*Cette fonction doit être définie hors d'une condition (if/else), donc on la définie avant de l'utiliser dans une boucle*/
+function test_input($data){
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if (isset($_POST['submit'])) {
+
+	$pseudo = $_POST["pseudo"];
+	$pwd = $_POST["motdepasse"];
+	
+
+  if (empty($_POST["pseudo"])){
+    $pseudoErr = "Il vous faut un pseudo";
+  }
+	else {
+		$pseudo = test_input($_POST["pseudo"]);
+		$pseudoErr = "";
+	}
+	if (empty($_POST["motdepasse"])) {
+		$pwdErr = "le mot de passe est requis";
+	}
+	else {
+		$pwd = test_input($_POST["motdepasse"]);
+	}
+	
+	$conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
+	
+	if(isset($_POST["pseudo"])){
+		if ((isset($pseudo)) &&( isset($pwd))){
+			$link=getConnection($dbHost, $dbUser, $dbPwd, $dbName);
+			if($_POST['dowpdown'] ==0){
+				if(getUserAdmin($pseudo, $pwd, $link) == 1) { 
+				$_SESSION["pseudo"]= $pseudo;
+				
+				header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/page_administrateur.php');
+				exit();
+			
+				}
+				else{
+					$pwdErr = "le pseudo/mot de passe sont erroné ou l'admin n'existe pas";
+				}
+			}
+			else{
+				if(getUserUtil($pseudo, $pwd, $link) == 1) { 
+					setConnectedUtil($pseudo, $link);
+					$_SESSION["pseudo"]= $pseudo;
+					
+					header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/page_utilisateur.php');
+					exit();
+				
+				}
+				else{
+					$pwdErr = "le pseudo/mot de passe sont erroné ou l'utilisateur n'existe pas";
+				}
+			}
+		}
+	}
+	
+}
+ 
+?>
+	<div class="row justify-content-center">
+	<div class="block container p-4 m-4 border rounded border-dark" name='block'>
+		<form action="connexion.php" method="POST">
+
+		<div class="row justify-content-start">
+			<div class="col-4">
+				<p>* champs requis</p>
+			</div>
+		</div>
+		<div  class="row justify-content-start p-2">
+			<div class="col-5" >
+				Selection type utilisateur*
+			</div>
+			<div class="col-6">
+			<form action="function.php" method="post">
+				<select name="dowpdown" >
+					<option value="0">Administrateur</option>
+					<option value="1" SELECTED>Utilisateur</option>
+				</select>
+			</form>
+			</div>
+		<div class="row justify-content-start p-2">
+			<div class="col-5 ">
+				Pseudo*
+			</div>
+			<div class="col-6 ">
+			  <input type="text" name="p" id="pseudo" placeholder="Pseudo">
+			</div>
+				<small class="col-10">
+					<?php
+						if(isset($pseudoErr) && $pseudoErr){
+							echo $pseudoErr;}
+					?>
+			  </small>
+			<div class="row justify-content-start p-2">
+				<div class="col-5 ">
+					Mot de passe*
+				</div>
+				<div class="col-6 ">
+				  <input type="password" name="motdepasse" id="motdepasse">
+				</div>
+					<small class="col-10">
+						<?php
+							if(isset($pwdErr) && $pwdErr){
+								echo $pwdErr;
+							}
+						?>
+				  </small class="col-10">
+				  </form>
+				<div style="display:flex; margin-top :3em;margin-left:9em; padding-right:8em;padding-left:2em;">
+				<form action="connexion.php" method="POST">
+						<input type="submit" name="submit" value="Se Connecter"/>
+						</form>
+				</div></br>
+				<div style="padding-left:2em;">
+				<a href="https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/inscription.php"> Pas encore Inscrit ? </a>
+				</div>
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
+<?php
+
+if (isset($_POST['accueil'])) {
+    header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/accueil.php');
+    exit();
+}
+
+?>
