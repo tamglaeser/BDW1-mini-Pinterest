64146a020a23b596409d17c13e2878b0f2e83af8
MASINGARBE MARINE p1501149 <marine.masingarbe@etu.univ-lyon1.fr>
2020-04-19 22:49:55 +0200

    la bonne fonction

--- sup.php
+++ sup.php
@@ -20,7 +20,7 @@ $ps = $_SESSION['pseudo'];
 </head>
 <body>
 <?php
-$conn = getConnection('localhost', "p1501149", "49afdf", "p1501149");
+$conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
 sup($pId,$ps);
 
 ?>
@@ -31,7 +31,7 @@ function sup($photoId,$pseudo) {
 				
 				if(isset($_SESSION['motdepasse'])){$pwd= $_SESSION['motdepasse'];}
 				echo "patat";
-				$conn = getConnection('localhost', "p1501149", "49afdf", "p1501149");
+				$conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
 				
 				if(getUserUtil($pseudo,$_SESSION['motdepasse'],$conn) ==1){
 					echo"y";
@@ -41,20 +41,20 @@ function sup($photoId,$pseudo) {
 						echo "eeeeeee";
 						executeUpdate($conn, "DELETE FROM Photo WHERE photoId =\"$photoId\"");
 						echo "la photo a ete supprimé";
-						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_utilisateur.php');
+						header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/page_utilisateur.php');
 						
 						exit();
 					}
 					else{
 						echo "vous ne pouvez pas la supprimer ";
-						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_utilisateur.php');
+						header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/page_utilisateur.php');
 					}
 				}
 			   else {
 					if(getUserAdmin($pseudo,$_SESSION['motdepasse'],$conn) ==1){
 						echo"sssssssssssssssssssss";
 						executeUpdate($conn, "DELETE FROM Photo WHERE photoId =\"$photoId\"");
-						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_administrateur.php');
+						header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/page_administrateur.php');
 						
 						exit();
 						echo "aaaa";
