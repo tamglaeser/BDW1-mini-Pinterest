d0d368a73275063e5942b781a5565b4242909ffb
MASINGARBE MARINE p1501149 <marine.masingarbe@etu.univ-lyon1.fr>
2020-04-19 22:47:57 +0200

    Replace sup.php

--- sup.php
+++ sup.php
@@ -1,24 +1,61 @@
-<?php function sup() {
+<?php 
+
+session_start();
+require_once ('bd.php');
+require_once ('administrateur.php');
+require_once ('utilisateur.php');
+require_once ('function.php');
+
+$pId = $_SESSION['photoId'];
+$ps = $_SESSION['pseudo'];
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PhotouCat</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="accueil.css">
+</head>
+<body>
+<?php
+$conn = getConnection('localhost', "p1501149", "49afdf", "p1501149");
+sup($pId,$ps);
+
+?>
+</body>
+</html>
+<?php
+function sup($photoId,$pseudo) {
+				
+				if(isset($_SESSION['motdepasse'])){$pwd= $_SESSION['motdepasse'];}
 				echo "patat";
-				if(getUserUtil($pseudo,$pwd,$conn) ==1){
+				$conn = getConnection('localhost', "p1501149", "49afdf", "p1501149");
+				
+				if(getUserUtil($pseudo,$_SESSION['motdepasse'],$conn) ==1){
 					echo"y";
 					$result_pseudo= executeQuery($conn, "SELECT utilId FROM utilisateur WHERE utilPseudo= \"$pseudo\"");
 					$result_photo= executeQuery($conn, "SELECT utilId FROM Photo WHERE photoId=\"$photoId\"");
 					if($result_photo== $result_pseudo){
+						echo "eeeeeee";
 						executeUpdate($conn, "DELETE FROM Photo WHERE photoId =\"$photoId\"");
-						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_utilisateur.php');?>
-						<script>txt.textContent='la Photo à bien été supprimée';</script><?php
+						echo "la photo a ete supprimé";
+						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_utilisateur.php');
+						
 						exit();
 					}
 					else{
-						?> <script> aff.textContent='Vous ne pouvez pas supprimer cette photo';</script><?php
+						echo "vous ne pouvez pas la supprimer ";
+						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_utilisateur.php');
 					}
 				}
 			   else {
-					if(getUserAdmin($pseudo,$pwd,$conn) ==1){
+					if(getUserAdmin($pseudo,$_SESSION['motdepasse'],$conn) ==1){
+						echo"sssssssssssssssssssss";
 						executeUpdate($conn, "DELETE FROM Photo WHERE photoId =\"$photoId\"");
-						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_administrateur.php');?>
-						<script>txt.textContent='la Photo à bien été supprimée';</script><?php
+						header('Location: https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/page_administrateur.php');
+						
 						exit();
 						echo "aaaa";
 					}
@@ -27,4 +64,5 @@
 					}
 				}
 			  
-			}?>
+	}?>
+			
