42e648c5afcea531631adbffcb399c2947e9f396
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-19 15:03:57 -0400

    debug

--- page_utilisateur.php
+++ page_utilisateur.php
@@ -18,7 +18,7 @@ $dir = "assets/images/";
 <body>
 <div style="background-image:url(img/accueil_bis.jpg);" ><B><h1>PhotouCat_Util</h1></B><br> </div>
 <nav class="crumbs">
-	<form name="accueil" action="accueil.php" method="POST">
+	<form name="accueil" action="page_utilisateur.php" method="post">
 	   <button style="float: left;" type="submit" name="accueil" class="btn btn-success">
 		Accueil
 		</button>
