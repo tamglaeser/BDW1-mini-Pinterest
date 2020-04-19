4e712e71d58b5eccf7c78962424bfdf8de35f9ee
MASINGARBE MARINE p1501149 <marine.masingarbe@etu.univ-lyon1.fr>
2020-04-19 22:50:13 +0200

    Replace details.php

--- details.php
+++ details.php
@@ -1,3 +1,17 @@
+<?php session_start();
+require_once ('bd.php');
+require_once ('administrateur.php');
+require_once ('utilisateur.php');
+require_once ('function.php');
+
+$photoId = $_GET['photoId'];
+$_SESSION['photoId']=$photoId;
+if(isset($_SESSION['pseudo'])){$pseudo = $_SESSION['pseudo'];}
+if(isset($_SESSION['motdepasse'])){$pwd = $_SESSION['motdepasse'];}
+
+
+$conn = getConnection('localhost', "p1501149", "49afdf", "p1501149");
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -7,20 +21,29 @@
     <link rel="stylesheet" href="accueil.css">
 </head>
 <body>
-
-
+<div style="background-image:url(img/accueil_bis.jpg);" ><B><h1>PhotoCat</h1></B><br> </div>
+	<nav class="crumbs">
+	<?php 
+	if(empty($_SESSION['pseudo']) && empty($_SESSION['motdepasse'])){
+		echo "<a href='accueil.php'>ACCUEIL</a>";
+	}else{
+		
+		if(getUserAdmin($pseudo, $pwd, $conn) == 1) { 
+			echo "<a href='page_administrateur.php'>ACCUEIL</a>";
+				
+		}else{
+					
+			if(getUserUtil($pseudo, $pwd, $conn) == 1) {
+				echo "<a href='page_utilisateur.php'>ACCUEIL</a>";
+					
+			}
+		}
+	}
+?>
+	</nav><br>
 
 <?php
-session_start();
-require_once ('bd.php');
-require_once ('administrateur.php');
-require_once ('utilisateur.php');
-require_once ('function.php');
-$photoId = $_GET['photoId'];
-$pseudo= $_SESSION['pseudo'];
-$pwd= $_SESSION['motdepasse'];
 
-$conn = getConnection('localhost', "p1926029", "ef5d0c", "p1926029");
 $dir = "assets/images/";
 
 details($photoId, $conn);
@@ -30,8 +53,11 @@ function details($ImageId, $link) {?>
     <?php
     $resultat_imNom = executeQuery($link, "SELECT nomFich FROM Photo WHERE photoId = $ImageId");
     $row_imNom = $resultat_imNom->fetch_assoc();
-    $images = glob($GLOBALS['dir'] . $row_imNom["nomFich"], GLOB_BRACE);
-    foreach ($images as $image):
+    $images = glob($GLOBALS['dir'] . $row_imNom["nomFich"], GLOB_BRACE);?>
+	<div class="row justify-content-center " style="margin:1em;padding:0;">
+   <div  class="row justify-content-start p-2">
+   <div class="col-4" >
+   <?php foreach ($images as $image):
         echo "<img src='" . $image . "' hspace = '10' border = '5'/>";
     endforeach;
 
@@ -44,8 +70,8 @@ function details($ImageId, $link) {?>
     $resultat_cat_Id = executeQuery($link, "SELECT catId FROM Photo WHERE photoId = $ImageId");
     $row_cat_id = $resultat_cat_Id->fetch_assoc();
     ?>
-
-
+	</div>
+	<div class="col-8" >
     <table>
         <tr>
             <th>Description</th>
@@ -72,29 +98,47 @@ function details($ImageId, $link) {?>
             </td>
         </tr>
     </table>
+	</div>
+	</div>
+	</div>
 	</br></br>
-		<form method="POST">
-			<input name="supression" id="sup" type="submit" value="supprimer la photo">
+		<?php 
+	if(empty($_SESSION['pseudo']) && empty($_SESSION['motdepasse'])){
+		
+	}else{
+		if(getUserAdmin($_SESSION['pseudo'], $_SESSION['motdepasse'], $link) == 1) { 
+			$pseudo = $_SESSION['pseudo'];?>
+			<div class="row justify-content-center p-2">
+			<a href="https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/sup.php"> supprimer la photo</a><br>
 			
-		</form>
+			
+			<a href="https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/modif.php"> modifier les détails </a><br>
+			</div>
+			<?php
+	
+		}else{
+					
+			if(getUserUtil($_SESSION['pseudo'], $_SESSION['motdepasse'], $link) == 1) { 
+
 		
-		
+			?>	<div class="row justify-content-center p-2">
+				<a href="https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/sup.php"> supprimer la photo</a><br>
+				<a href="https://bdw1.univ-lyon1.fr/p1501149/bdw1_projet-master/modif.php"> modifier les détails </a><br>
+				</div>
+				<?php
+				
+					
+			}
+		}
+	}
+
 			
 
 		
-    <?php
-}	
-//$url = htmlspecialchars($_SERVER['HTTP_REFERER']);?><script>
-			var btn = document.querySelector('input');
-			var aff = document.querySelector('p');
-		</script>	<?php
-			echo "p";
-			 if (isset($_POST['supression'])) {
-				 include("sup.php");
-			 }
-			?>
+    
+}	?>
 
 
-
+		
 </body>
 </html>
