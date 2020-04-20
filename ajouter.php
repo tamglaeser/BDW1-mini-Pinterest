b528ae6db530ea87f85a5436fd98091a1c0f350d
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-19 23:38:23 -0400

    debug ajouter fichier

--- ajouter.php
+++ ajouter.php
@@ -18,7 +18,9 @@ $uploadOk = 1;
 //$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
+
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    echo "FILE: ". $target_file;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
