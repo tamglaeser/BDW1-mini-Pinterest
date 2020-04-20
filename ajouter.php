19f818edfff301b2b5b7cb8c03e57fc67fa34f88
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-20 12:58:00 -0400

    update

--- ajouter.php
+++ ajouter.php
@@ -4,7 +4,7 @@
 
 <form action="ajouter.php" method="post" enctype="multipart/form-data">
     Choisir le fichier:
-    <input type="file" name="Parcourir.." id="fileToUpload">
+    <input type="file" value="Parcourir.." name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Envoyer" name="submit">
 </form>
 
@@ -19,10 +19,10 @@ $uploadOk = 1;
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
 
-    $target_file = $target_dir . basename($_FILES["Parcourir.."]["name"]);
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    $check = getimagesize($_FILES["Parcourir.."]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -37,7 +37,7 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 // Check file size
-    if ($_FILES["Parcourir.."]["size"] > 100000) {
+    if ($_FILES["fileToUpload"]["size"] > 100000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
@@ -51,8 +51,8 @@ if(isset($_POST["submit"])) {
         echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["Parcourir.."]["tmp_name"], $target_file)) {
-            echo "The file " . basename($_FILES["Parcourir.."]["name"]) . " has been uploaded.";
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
