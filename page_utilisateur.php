1865bd0e989622e1d4a25988d0d75a82df48b8db
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-19 20:48:57 -0400

    clean up

--- page_utilisateur.php
+++ page_utilisateur.php
@@ -39,9 +39,7 @@ $dir = "assets/images/";
     <!-- here start the dropdown list -->
 	<div style='display:flex; margin-left:5em;'>
         <?php
-        echo "pseudo: ".$_GET['pseudo'];
-        //$resultat_util = executeQuery($GLOBALS['conn'], "SELECT utilId FROM utilisateur WHERE utilPseudo='" . $_GET['pseudo'] . "'");
-        $resultat_cat = executeQuery($GLOBALS['conn'], "SELECT c.nomCat FROM Categorie c JOIN Photo p ON p.catID=c.catID WHERE p.utilId IN (SELECT utilId FROM utilisateur WHERE utilPseudo='" . $_GET['pseudo'] . "')");
+        $resultat_cat = executeQuery($GLOBALS['conn'], "SELECT DISTINCT c.nomCat FROM Categorie c JOIN Photo p ON p.catID=c.catID WHERE p.utilId IN (SELECT utilId FROM utilisateur WHERE utilPseudo='" . $_GET['pseudo'] . "')");
         ?>
         <select name="dowpdown" >
             <option value="0">Toutes les photos</option>
