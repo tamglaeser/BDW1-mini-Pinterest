e73103d83979b6a81b9a78ae269f827378c8a654
Tullia Glaeser <tullia.vinti@gmail.com>
2020-04-26 16:36:51 -0400

    debug

--- caches.php
+++ caches.php
@@ -23,15 +23,17 @@ $dir = "assets/images/";
     <body>
     <div style="background-image:url(img/accueil_bis.jpg);" ><B><h1>PhotouCat_Util</h1></B><br>
         <?php if (!isset($_SESSION['pseudo'])) {
+            echo "entered caches but if !isset session pseudo";
             echo "Please Login again";
-            header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/connexion.php');
+            //header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/connexion.php');
         }
         else {
             $now = time(); // Checking the time now when home page starts.
 
             if ($now > $_SESSION['expire']) {
+                echo "entered caches but there is session pseudo but time expired";
                 session_destroy();
-                header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/connexion.php');
+                //header('Location: https://bdw1.univ-lyon1.fr/p1926029/BDW1-ProjetFinale/bdw1_projet/connexion.php');
             }
             else { //Starting this else one [else1]
                 ?>
